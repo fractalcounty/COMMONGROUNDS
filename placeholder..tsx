3e5e7ba4599270282523cc947f546a3f93c63bee
fractalcounty <contact@fractalcounty.com>
2023-12-20 02:40:30 -0600

    .gitignore cleanup

--- placeholder..tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="placeholder" tilewidth="64" tileheight="64" tilecount="117" columns="13">
- <image source="resources/sprites/tilemap_temp.png" width="892" height="576"/>
-</tileset>
