8289c0e15671d5c33e20d0613e742571582eca9c
fractalcounty <contact@fractalcounty.com>
2023-12-20 22:58:53 -0600

    Project structure, rolus fart

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -1,26 +1,31 @@
 extends Node
 
-## General launch arguments
-## Todo: allow OS-level launch args
-const SKIP_INTRO : bool = true
+## General game properties
+# TODO: Change these from exports to executable launch arguments
+@export var SKIP_INTRO : bool = true
 
-## SceneManager.gd constants
-## Defines scnees loaded by the SceneManager singleton
-const MAP_PATH : String = "res://scenes/_debug/Scene2.tscn"
-const MAIN_MENU_PATH = preload("res://scenes/menu/MainMenu.tscn")
-const SPLASH_SCREEN_PATH = preload("res://scenes/menu/Splash.tscn")
-const INTERFACE_POST_PROCESSING = preload("res://scenes/menu/PostProcessing.tscn")
+#region Autoload Properties (res://src)
+@export_category("Autoload Properties")
 
-## SoundManager.gd constants
-const UI_SOUND_BUS : String = "Interface"
-const WORLD_SOUND_BUS : String = "World"
+@export_subgroup("SceneManager")
+@export var MAP_PATH : String = "res://scenes/world/map/Map.tscn"
+@export var MAIN_MENU_PATH = preload("res://scenes/interface/menus/MainMenu.tscn")
+@export var SPLASH_SCREEN_PATH = preload("res://scenes/interface/menus/Splash.tscn")
+@export var INTERFACE_POST_PROCESSING = preload("res://scenes/interface/menus/PostProcessing.tscn")
+
+@export_subgroup("SoundManager")
+@export var SOUND_BUS : String = "Sound"
+@export var INTERFACE_SOUND_BUS : String = "Interface"
+@export var MUSIC_BUS : String = "Music"
+#endregion
 
 ## Initialization of game
 func _ready() -> void:
 	GameManager.game = self
 	GameManager.game_ready.emit()
 	
-	SoundManager.set_default_sound_bus(WORLD_SOUND_BUS)
-	SoundManager.set_default_ui_sound_bus(UI_SOUND_BUS)
-	
 	SceneManager.load_game(SKIP_INTRO)
+	
+	SoundManager.set_default_sound_bus(SOUND_BUS)
+	SoundManager.set_default_music_bus(MUSIC_BUS)
+	SoundManager.set_default_ui_sound_bus(INTERFACE_SOUND_BUS)
