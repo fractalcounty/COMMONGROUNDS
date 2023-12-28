534420d327d970fcd855405c829a52589b42bd27
Jason <54966874+JasonTGF@users.noreply.github.com>
2023-12-27 17:37:41 -0800

    Pulling data from NG art frontpage using custom API
    YaY!

---
+++ game/scenes/TestApiScene/ApiConnection.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _Get_Api_Data():
+	$HTTPRequest.request("https://jsonplaceholder.typicode.com/todos/1")
+	pass # Replace with function body.
+
+
+func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	print(json.result)
+	pass # Replace with function body.
