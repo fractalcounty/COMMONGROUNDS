906002913c01e3f2611d24ebf8cf573a4c6ce0dd
Jason <54966874+JasonTGF@users.noreply.github.com>
2023-12-27 18:13:22 -0800

    prints only necessary data

--- game/scenes/TestApiScene/ApiButton.gd
+++ game/scenes/TestApiScene/ApiButton.gd
@@ -4,12 +4,15 @@ extends Button
 const URL = "https://api.browse.ai/v2/robots/ac0415f8-bde8-44f7-bb1f-3bfca674ff4a/tasks/4dd6e621-9a8a-419e-b4f5-549694f2aed9"
 const headers = ["Authorization: Bearer b4a2a50f-9205-471f-9d3a-66cf39014983:218765be-de49-4604-979c-77416f17b084"]
 
+#signal info(data)
+
 func _on_pressed() -> void:
 	http_request.request(URL, headers)
 
-
-
 func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	var data = JSON.parse_string(body.get_string_from_utf8())
-	print(data)
+	var rawData = JSON.parse_string(body.get_string_from_utf8())
+
+	var data = rawData.result
+	var capturedTexts = data["capturedTexts"]
+	print(capturedTexts)
 
